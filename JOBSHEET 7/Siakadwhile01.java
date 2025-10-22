c768b46ac4362e5348be88e5461886e32f7a5129
adzrielllag-rgb <adzrielllag@gmail.com>
2025-10-22 11:25:55 +0700

    percobaan 1

---
+++ JOBSHEET 7/Siakadwhile01.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+public class Siakadwhile01 {
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner (System.in);
+    int i = 0, nilai, jml;
+    
+    System.out.print("Masukkan jumlah mahasiswa : ");
+    jml = sc.nextInt();
+    while (i < jml) {
+      System.out.print("Masukkan nilai mahasiswa ke-" + (i+1) + " : ");
+      nilai = sc.nextInt();
+
+      if (nilai < 0 || nilai > 100) {
+        System.out.println("Nilai yang dimasukkan tidak valid!");
+        continue;
+      }
+
+      if (nilai > 80 && nilai <= 100) {
+        System.out.println("Nilai mahasiswa ke-" + (i+1) + " : Grade A");
+      } else if (nilai > 73 && nilai <= 80) {
+        System.out.println("Nilai mahasiswa ke-" + (i+1) + " : Grade B+");
+      } else if (nilai > 65 && nilai <= 73) {
+        System.out.println("Nilai mahasiswa ke-" + (i+1) + " : Grade B");
+      } else if (nilai > 60 && nilai <= 65) {
+        System.out.println("Nilai mahasiswa ke-" + (i+1) + " : Grade C+");
+      } else if (nilai > 50 && nilai <= 60) {
+        System.out.println("Nilai mahasiswa ke-" + (i+1) + " : Grade C");
+      } else if (nilai > 39 && nilai <= 50) {
+        System.out.println("Nilai mahasiswa ke-" + (i+1) + " : Grade D");
+      } else {
+        System.out.println("Nilai mahasiswa ke-" + (i+1) + " : Grade E");
+      }
+      i++;
+    }
+  
+}
+}
+if (nilai > 60) {
+            lulus++;
+          } else {
+            tidakLulus++;
+          }
