1e43fb6f13de73e09cf4f571f3fe95e4553d4566
adzrielllag-rgb <adzrielllag@gmail.com>
2025-10-22 17:57:48 +0700

    percobaan 3

---
+++ JOBSHEET 7/kafeDoWhile01.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class kafeDoWhile01 {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int kopi,teh,roti;
+    double hargaKopi = 12000, hargaTeh = 7000, hargaRoti = 20000, totalHarga;
+    String nama_pelanggan;
+
+    do {
+      System.out.print("Masukkan nama pelanggan (ketik 'batal' untuk keluar  ) : ");
+      nama_pelanggan = sc.nextLine();
+      
+      if (nama_pelanggan.equalsIgnoreCase("batal")) {
+        System.out.println("Transaksi dibatalkan.");
+        break;
+      }
+      System.out.print("Masukkan jumlah kopi: ");
+      kopi = sc.nextInt();
+
+      System.out.print("Masukkan jumlah teh: ");
+      teh = sc.nextInt();
+      
+      System.out.print("Masukkan jumlah roti: ");
+      roti = sc.nextInt();
+
+      totalHarga = (kopi * hargaKopi) + (teh * hargaTeh) + (roti * hargaRoti);
+      System.out.println("Total  yang harus dibbayar : " +  totalHarga);
+
+    } while (true);
+
+      System.out.println("semua transaksi selesai.");
+    sc.close();
+  }
+  
+}
