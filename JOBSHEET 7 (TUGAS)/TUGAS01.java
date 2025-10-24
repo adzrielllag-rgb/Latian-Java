5a971135da714153e915b84f19b4d68544121c22
adzrielllag-rgb <adzrielllag@gmail.com>
2025-10-24 13:56:38 +0700

    JAWABAN PERTANYAAN PERCOBAAN 1,2,3

---
+++ JOBSHEET 7 (TUGAS)/TUGAS01.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class TUGAS01 { 
+  public static void main(String[] args) {
+    
+    Scanner sc = new Scanner(System.in);
+    double hargaTiket=50000, totalBayar;
+    int jumlahTiket;
+
+    System.out.print("Masukkan jumlah tiket: ");
+    jumlahTiket = sc.nextInt();
+  }
+  
+}
