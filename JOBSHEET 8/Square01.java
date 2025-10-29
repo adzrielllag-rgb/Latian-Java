76ffd2a41ce6eb92259429c06c9b7efa3cfc4de6
adzrielllag-rgb <adzrielllag@gmail.com>
2025-10-29 17:09:50 +0700

    Percobaan 1

---
+++ JOBSHEET 8/Square01.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Square01 {
+
+  public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Masukkan nilai N = ");
+    int n = sc.nextInt();
+
+    for (int i = 1; i <= n; i++) {
+      for (int s = 1; s <= n; s++) {
+        System.out.print("*");
+      }
+      System.out.println();
+    }
+    sc.close();
+  }
+}
