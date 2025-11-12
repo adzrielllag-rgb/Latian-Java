1d4b469cd24dfde5c7ca7764682ad243511b3af7
adzrielllag-rgb <adzrielllag@gmail.com>
2025-11-12 11:54:39 +0700

    Tugas 1

---
+++ JOBSHEET 9 (TUGAS)/Tugas1_01.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+
+public class Tugas1_01 {
+  public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in) ;
+
+    int jmlNilai[] ;
+    int nilai, total = 0, nilai_tertinggi = 0 , nilai_terendah = 100 ;
+    double rata ;
+
+
+    System.out.print("Masukan jumlah nilai yang akan diinput : ");
+    int jmlInput = sc.nextInt() ;
+
+    jmlNilai = new int[jmlInput];
+
+    for (int i = 0; i < jmlInput ; i++) {
+      System.out.print("Masukan nilai ke-" + i + " : ");
+      nilai = sc.nextInt() ;
+      jmlNilai[i] = nilai ;
+
+    }
+
+    System.out.println() ;
+
+
+    for (int i = 0; i < jmlInput ; i++) {
+
+      System.out.println("nilai ke-" + i + " : " + jmlNilai[i]);
+      total += jmlNilai[i] ;
+      
+    }
+
+    for (int i = 0; i < jmlInput ; i++) {
+
+      if (jmlNilai[i] > nilai_tertinggi) {
+        nilai_tertinggi = jmlNilai[i] ;
+      }
+      if (jmlNilai[i] < nilai_terendah) {
+        nilai_terendah = jmlNilai[i] ;
+      }
+
+    }
+
+    System.out.println() ;
+    System.out.println("Nilai tertinggi adalah : " + nilai_tertinggi);
+    System.out.println("Nilai terendah adalah : " + nilai_terendah);
+
+    rata =  total / jmlInput ;
+    System.out.println("Rata-rata nilai adalah : " + rata);
+
+    
+   sc.close();
+    
+  }
+  
+}
