a958319c8df8692107794d75bcd65613aba31018
adzrielllag-rgb <adzrielllag@gmail.com>
2025-11-14 15:45:24 +0700

    MODIFIKASI PERTAMA PERCOBAAN 4

--- JOBSHEET 9/SearchNilai01.java
+++ JOBSHEET 9/SearchNilai01.java
@@ -2,22 +2,42 @@ import java.util.Scanner;
 
 public class SearchNilai01 {
   public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in) ;
 
-    int arrNilai [] = {80,85,78,96,90,82,86} ;
-    int key = 90 ;
+    int arrNilai []  ;
+    int key ;
     int hasil = 0 ;
+
+    System.out.print("Masukan niali yang akan diinput : ");
+    int n = sc.nextInt() ;
+    arrNilai = new int[n] ;
+
+    for (int i = 0; i < arrNilai.length ; i++) {
+      System.out.print("Masukan nilai Mahasiswa ke-" + i + " : ");
+      arrNilai[i] = sc.nextInt();
+    }
+
+    System.out.println();
+
+    System.out.print("Masukan nilai yang akan dicari : ");
+    key = sc.nextInt() ;
 
     for (int i = 0; i < arrNilai.length ; i++) {
 
       if (key == arrNilai[i]) {
         hasil = i ;
-        break ;
-      }
-    }
+        
+        
+        System.out.println("Nilai " +key + ", merupakan nilai mahasiswa  ke-" +hasil);
+ 
+      
 
-    System.out.println();
-    System.out.println("Nilai " +key + " ditemukan pada indeks ke-" +hasil);
-    System.out.println();
+    }
+    
+
   }
+
+
   
+}
 }
