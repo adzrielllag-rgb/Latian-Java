5c0c658d19c290529ad36c13375a95a83f87214e
adzrielllag-rgb <adzrielllag@gmail.com>
2025-11-19 10:06:14 +0700

    Tambahan Percobaan 3

--- JOBSHEET 10/Numbers01.java
+++ JOBSHEET 10/Numbers01.java
@@ -11,7 +11,10 @@ public class Numbers01 {
 
     for (int i = 0 ; i < myNumbers.length; i++) {
       System.out.println(Arrays.toString(myNumbers[i])) ;
-  }
+    }
   
+    for ( int i = 0 ; i < myNumbers.length ; i++) {
+      System.out.println("Panjang baris ke-" + (i+1) + " : " + myNumbers[i].length ) ;
+    }
  }
 }
