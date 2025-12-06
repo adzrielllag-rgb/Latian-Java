513917c3f577b1daaf6f9146911c122eb7aed849
adzrielllag-rgb <adzrielllag@gmail.com>
2025-12-07 00:56:30 +0700

    Modif Percobaan 4

--- JOBSHEET 11/pengunjungCafe01.java
+++ JOBSHEET 11/pengunjungCafe01.java
@@ -1,20 +1,40 @@
+import java.util.Scanner;
+
 public class pengunjungCafe01 {
 
-  static void daftarpengunjung (String...namaPengunjung) { 
-
-    System.out.println("Daftar nama  pengunjung : ");
-    for (int i = 0 ; i < namaPengunjung.length ; i++ ) {
-      System.out.println("- " + namaPengunjung[i]);
+  static int banyak(int jumlahPengunjung ) {
+    
+   return jumlahPengunjung;
     }
+  
+  static void namaPengunjung(String [] nama, int jumlahPengunjung) {
 
-  }
-
+    Scanner sc = new Scanner(System.in);
+  
+    for (int i = 0 ; i < jumlahPengunjung ; i++ ) {
+      System.out.print("Masukkan nama pengunjung ke-" + (i+1) + ": ");
+      nama [i]= sc.nextLine();
+    }
+  }   
   public static void main(String[] args) {
 
-  
-    daftarpengunjung("Ali","Budi","Citra") ;
-    daftarpengunjung("Andi");
-    daftarpengunjung("Doni","Eti","Fahmi","Galih");
+    Scanner sc = new Scanner(System.in);
+
+    System.out.print("Masukkan banyaknya pengunjung cafe hari ini: ");
+    int banyak = sc.nextInt();
+    sc.nextLine();
+
+    String [] daftarPengunjung = new String[banyak];
+    
+    System.out.println("Masukkan nama pengunjung: ");
+    namaPengunjung(daftarPengunjung, banyak);
+
+    System.out.println("\nDaftar nama pengunjung cafe hari ini: ");
+    for ( int i = 0 ; i < banyak ; i++ ) {
+      System.out.println("Pengunjung ke-" + (i+1) + ": " + daftarPengunjung[i]);
+    }
+    sc.close();
+    
   }
   
 }
